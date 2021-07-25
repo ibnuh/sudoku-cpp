932ff7c6feb7accad339b0c42a43d0a63ad3270c
Muhammad Ibnuh <ibnuhdev@gmail.com>
2021-07-25 21:15:25 +0700

    Use rlutil as rendering engine

--- sudoku.cpp
+++ sudoku.cpp
@@ -6,6 +6,8 @@
 
 #include <iomanip>
 
+#include "donkey.h"
+
 using namespace std;
 
 #define KEY_UP 72
@@ -14,41 +16,62 @@ using namespace std;
 #define KEY_RIGHT 77
 
 void render(int playground[9][9], int x, int y) {
-    system("cls");
+    donkey::cls();
 
     cout << "   _____           _       _          " << endl;
     cout << "  / ____|         | |     | |         " << endl;
     cout << " | (___  _   _  __| | ___ | | ___   _ " << endl;
     cout << "  \\___ \\| | | |/ _` |/ _ \\| |/ / | | |" << endl;
     cout << "  ____) | |_| | (_| | (_) |   <| |_| | by M. IBNU LUTHFI AZHAR " << endl;
-    cout << " |_____/ \\__,_|\\__,_|\\___/|_|\\_\\\\__,_| 200401067" << endl;
+    cout << " |_____/ \\__,_|\\__,_|\\___/|_|\\_\\\\__,_| 200401067" << endl << endl;
+    
+    cout << " /-----------------/-----------------/-----------------/" << endl;
+	cout << " | .5. |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " /-----------------/-----------------/-----------------/" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " /-----------------/-----------------/-----------------/" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " -------------------------------------------------------" << endl;
+	cout << " |     |     |     |     |     |     |     |     |     |" << endl;
+	cout << " /-----------------/-----------------/-----------------/" << endl;
+	
+	const int initX = 4, initY = 9, gapX = 6, gapY = 2;
 
-    cout << endl << " /-----------------/-----------------/-----------------/" << endl;
     for (int row = 0; row < 9; row++) {
-        cout << " |  ";
+    	int screenY = initY + (row * gapY);
+    	
         for (int column = 0; column < 9; column++) {
+        	int screenX = initX + (column * gapX);
             int currentNumber = playground[row][column];
-
-            if (currentNumber == 0) {
-                if (x == column && y == row) {
-                    cout << "_  |  ";
-                } else {
-                    cout << "   |  ";
-                }
-            } else {
-                if (x == column && y == row) {
-                    cout << setw(1) << currentNumber << "*" << setw(4);
-                    cout << "|  ";
-                } else {
-                    cout << setw(1) << currentNumber << "  |  " << setw(2);
-                }
+            
+            donkey::locate(screenX, screenY);
+            
+            if (x == column && y == row) { // Check if cursor is in current block
+            	if(currentNumber == 0) { // Treat 0 as empty block
+            		cout << " _ ";
+				}
+				else {
+					cout << "_" << currentNumber << "_";
+				}
             }
-        }
-
-        if ((row + 1) % 3 == 0) {
-            cout << endl << " /-----------------/-----------------/-----------------/" << endl;
-        } else {
-            cout << endl << " -------------------------------------------------------" << endl;
+            else {
+            	if(currentNumber == 0) { // Treat 0 as empty block
+            		cout << "   ";
+				}
+				else {
+					cout << " " << currentNumber << " ";
+				}
+			}
         }
     }
 }
@@ -76,23 +99,23 @@ int main() {
         int buttonCode = 0;
 
         switch (buttonCode = getch()) {
-        case KEY_UP:
-            if (y != 0) y--;
-            break;
-        case KEY_DOWN:
-            if (y != 8) y++;
-            break;
-        case KEY_LEFT:
-            if (x != 0) x--;
-            break;
-        case KEY_RIGHT:
-            if (x != 8) x++;
-            break;
-        default:
-            if (buttonCode >= 48 && buttonCode <= 57) {
-                playground[y][x] = buttonCode - '0'; // Convert ascii number to a real character
-            }
-            break;
+	        case KEY_UP:
+	            if (y != 0) y--;
+	            break;
+	        case KEY_DOWN:
+	            if (y != 8) y++;
+	            break;
+	        case KEY_LEFT:
+	            if (x != 0) x--;
+	            break;
+	        case KEY_RIGHT:
+	            if (x != 8) x++;
+	            break;
+	        default:
+	            if (buttonCode >= 48 && buttonCode <= 57) {
+	                playground[y][x] = buttonCode - '0'; // Convert ascii number to a real character
+	            }
+	            break;
         }
 
         render(playground, x, y);
